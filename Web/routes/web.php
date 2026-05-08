35db69e7c4e4733968e53d1c3730d7d5717513c4
Ferdy P <pratamaperdy92@gmail.com>
2026-05-08 08:35:50 +0700

    update log

--- Web/routes/web.php
+++ Web/routes/web.php
@@ -259,3 +259,6 @@ Route::fallback(function () {
 
     return redirect()->route('login');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
--- Web/vite.config.js
+++ Web/vite.config.js
@@ -5,10 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/css/admin/admin.css',
+                'resources/sass/app.scss',
                 'resources/js/app.js',
-                'resources/js/admin.js',
             ],
             refresh: true,
         }),

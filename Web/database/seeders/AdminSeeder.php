136568a3b528dd8391a2720fbe5968f1261ddd42
pimmrajaibelis <pimmrajaibelis@gmail.com>
2026-05-12 10:56:46 +0700

    update

--- Web/database/seeders/AdminSeeder.php
+++ Web/database/seeders/AdminSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class AdminSeeder extends Seeder
 {
@@ -12,6 +14,16 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            'nama' => 'RuangKita',
+            'email' => 'ruangkitasmkn64gmail.com',
+            'password' => 'adminruangkita',
+            'role' => 'admin',
+            'no_hp' => '085220007279',
+            'alamat' => 'none',
+            'email_verified_at' => now(),
+            'created_at' => now(),
+            "updated_at" => now(),
+        ]);
     }
 }
